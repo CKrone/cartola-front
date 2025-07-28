928d2bc17645d397ff06e99a7fd567e4ecc38bd5
Cristian Krone <cristian_krone@hotmail.com>
2025-07-28 11:00:11 -0300

    FEAT[FRONT] Corrigido formatação do valor ganho por rodada. Adicionado propriedade para controlar a diferença entre o competidor e o próximo e a diferença entre o competidor e o primeiro colocado. Criado constantes para armazenar os valores das premiações.

--- src/components/ListPlayers.vue
+++ src/components/ListPlayers.vue
@@ -4,18 +4,21 @@ import CryptoJS from 'crypto-js'
 import supabase from '@/supaBaseClient.js'
 
 const players = ref([
-    { id: 1, name: 'Cassiano', wins: 0, lastRoundPoints: 0, totalPoints: 0, valuePerRound: 0, valueTotal: 0 },
-    { id: 2, name: 'Cristian', wins: 0, lastRoundPoints: 0, totalPoints: 0, valuePerRound: 0, valueTotal: 0 },
-    { id: 3, name: 'Jean', wins: 0, lastRoundPoints: 0, totalPoints: 0, valuePerRound: 0, valueTotal: 0 },
-    { id: 4, name: 'Jhony', wins: 0, lastRoundPoints: 0, totalPoints: 0, valuePerRound: 0, valueTotal: 0 },
-    { id: 5, name: 'Paulo', wins: 0, lastRoundPoints: 0, totalPoints: 0, valuePerRound: 0, valueTotal: 0 },
-    { id: 6, name: 'Valdir', wins: 0, lastRoundPoints: 0, totalPoints: 0, valuePerRound: 0, valueTotal: 0 }
+    { id: 1, name: 'Cassiano', wins: 0, lastRoundPoints: 0, totalPoints: 0, valuePerRound: 0, valueTotal: 0, pointsToNext: 0, pointsToLeader: 0 },
+    { id: 2, name: 'Cristian', wins: 0, lastRoundPoints: 0, totalPoints: 0, valuePerRound: 0, valueTotal: 0, pointsToNext: 0, pointsToLeader: 0 },
+    { id: 3, name: 'Jean', wins: 0, lastRoundPoints: 0, totalPoints: 0, valuePerRound: 0, valueTotal: 0, pointsToNext: 0, pointsToLeader: 0 },
+    { id: 4, name: 'Jhony', wins: 0, lastRoundPoints: 0, totalPoints: 0, valuePerRound: 0, valueTotal: 0, pointsToNext: 0, pointsToLeader: 0 },
+    { id: 5, name: 'Paulo', wins: 0, lastRoundPoints: 0, totalPoints: 0, valuePerRound: 0, valueTotal: 0, pointsToNext: 0, pointsToLeader: 0 },
+    { id: 6, name: 'Valdir', wins: 0, lastRoundPoints: 0, totalPoints: 0, valuePerRound: 0, valueTotal: 0, pointsToNext: 0, pointsToLeader: 0 }
 ])
 
 let loading = ref(false)
 
 const codeInput = ref('')
 const isCodeValid = ref(false)
+const BONIFICACAO_PRIMEIRO_COLOCADO = 200
+const BONIFICACAO_SEGUNDO_COLOCADO = 130
+const BONIFICACAO_TERCEIRO_COLOCADO = 80
 const checkCodeValidity = async () => {
     const inputHash = CryptoJS.SHA256(codeInput.value).toString(CryptoJS.enc.Hex)
 
@@ -52,6 +55,8 @@ const headers = [
     { title: 'Vitórias', key: 'wins' },
     { title: 'Última Rodada', key: 'lastRoundPoints' },
     { title: 'Pontuação Total', key: 'totalPoints' },
+    { title: 'Pontos Até o Próximo ', key: 'pointsToNext' },
+    { title: 'Pontos Até o Líder', key: 'pointsToLeader' },
     { title: 'Valor Ganho Por Rodada', key: 'valuePerRound' },
     { title: 'Total Premiação', key: 'valueTotal' }
 ]
@@ -90,9 +95,9 @@ const calculateValueTotal = (player) => {
     const sortedByPoints = [...players.value].sort((a, b) => b.totalPoints - a.totalPoints)
     const rank = sortedByPoints.findIndex(p => p.name === player.name)
     let bonus = 0
-    if (rank === 0) bonus = 200
-    else if (rank === 1) bonus = 130
-    else if (rank === 2) bonus = 80
+    if (rank === 0) bonus = BONIFICACAO_PRIMEIRO_COLOCADO
+    else if (rank === 1) bonus = BONIFICACAO_SEGUNDO_COLOCADO
+    else if (rank === 2) bonus = BONIFICACAO_TERCEIRO_COLOCADO
     return parseFloat(player.valuePerRound) + bonus
 }
 
@@ -106,8 +111,29 @@ const openModal = () => {
 
 const savePoints = () => {
     updateAllPlayersStats(roundPointsList.value)
+    calculatePointsDifference(players)
     savePlayerData(players)
     modalVisible.value = false
+}
+
+const calculatePointsDifference = (players) => {
+    const sorted = [...players.value].sort((a, b) => b.totalPoints - a.totalPoints)
+    const leaderPoints = sorted[0]?.totalPoints || 0
+
+    sorted.forEach((player, index) => {
+        const currentPoints = player.totalPoints
+        const nextPlayerPoints = sorted[index - 1]?.totalPoints
+
+        player.pointsToLeader = parseFloat((leaderPoints - currentPoints).toFixed(2))
+        player.pointsToNext = index === 0
+            ? 0
+            : parseFloat((nextPlayerPoints - currentPoints).toFixed(2))
+    })
+
+    players.value = players.value.map(p => {
+        const updated = sorted.find(s => s.id === p.id)
+        return updated ? { ...p, ...updated } : p
+    })
 }
 
 const savePlayerData = async (players) => {
@@ -149,6 +175,8 @@ onMounted(async () => {
             totalPoints: player.totalPoints || 0,
             valuePerRound: player.valuePerRound || 0,
             valueTotal: player.valueTotal || 0,
+            pointsToNext: player.pointsToNext || 0,
+            pointsToLeader: player.pointsToLeader || 0,
             wins: player.wins || 0
         }))
     } catch (error) {
@@ -171,11 +199,17 @@ onMounted(async () => {
             :items-per-page-options="[10,15,25]"
             :hide-default-footer="true"
         >
-            <template v-slot:item.value="{ item }">
+            <template v-slot:item.valuePerRound="{ item }">
                 <span>{{ formatCurrency(item.valuePerRound) }}</span>
             </template>
             <template v-slot:item.valueTotal="{ item }">
                 <span>{{ formatCurrency(item.valueTotal) }}</span>
+            </template>
+            <template v-slot:item.pointsToNext="{ item }">
+                {{ item.pointsToNext.toFixed(2) }}
+            </template>
+            <template v-slot:item.pointsToLeader="{ item }">
+                {{ item.pointsToLeader.toFixed(2) }}
             </template>
             <template v-slot:item.totalPoints="{ item }">
                 {{ item.totalPoints.toFixed(2) }}
